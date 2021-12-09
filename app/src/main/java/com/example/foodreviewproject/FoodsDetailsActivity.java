736ea0cac2bfc0ec652abf7360f40d634446c6c0
NhatNguyen150285 <nhatnguyen150285@gmail.com>
2021-12-09 13:42:35 +0700

    Pro

---
+++ app/src/main/java/com/example/foodreviewproject/FoodsDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodreviewproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foods_details);
+    }
+}
